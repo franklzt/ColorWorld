d6fae4d7553668aa1ae7604f9f3ef46991acc71b
franklzt <289663346@qq.com>
2020-01-03 16:24:25 +0800

    Add Basic info about music

---
+++ Assets/Code/MusicItem.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicItem : MonoBehaviour
+{
+    public Text musicName;
+    public Text numberName;
+    public Text singName;
+}
---
+++ Assets/Code/MusicNameController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicNameController : MonoBehaviour
+{
+    public MusicItem prefab;
+    public Transform rootParent;
+    public Text randomText;
+    public Button nextGenerateBtn;
+    public Button toggleRootBtn;
+
+    string[] musicNames = { "A", "B", "C", "D", "E", "F", "G" };
+    string[] numberNames = { "1", "2", "3", "4", "5", "6", "7" };
+    string[] singNames = { "Do", "Lei", "Mi", "Fa", "So", "La", "Si" };
+
+    
+    void Start()
+    {
+        nextGenerateBtn.onClick.AddListener(() =>
+        {
+            GenerateText();
+        });
+
+        toggleRootBtn.onClick.AddListener(()=>
+        {
+            rootParent.gameObject.SetActive(!rootParent.gameObject.activeInHierarchy);
+        });
+
+
+        for (int i = 0; i < 7; i++)
+        {
+            MusicItem musicItem = Instantiate<MusicItem>(prefab, rootParent);
+            musicItem.musicName.text = musicNames[i];
+            musicItem.numberName.text = numberNames[i];
+            musicItem.singName.text = singNames[i];
+        }
+        GenerateText();
+    }
+
+    void GenerateText()
+    {
+        int random = Random.Range(0, 7);
+        randomText.text = numberNames[random];
+    }
+}
